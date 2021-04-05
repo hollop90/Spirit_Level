8287f5f7008eb2018c7ce48cf3b8cea22dd06be0
Ugo <nozolo96@gmail.com>
2021-04-05 16:03:10 +0100

    got the menu system working

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,9 @@
 #include "NewPing.h"
 #include "LiquidCrystal_I2C.h"
 
-#define IMU_DEBUG
+#include <Button Lib.h>
+
+//#define IMU_DEBUG
 #define OUTPUT_DEBUG
 
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
@@ -27,10 +29,22 @@ MPU6050 mpu;
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
+bool singlePress = true;
+bool switchMenu = false;
 int menuState = 0;
+
+bool init0 = false;
+bool init1 = false;
+
+void menu0();
+void menu1();
+void rangeError();
+
+Button switchA(6, true);
 
 // Distance sensor vars
 int distance;
+int distanceBuff;
 unsigned long sonarInterval = 50;
 unsigned long lastPingTime;
 
@@ -54,59 +68,69 @@ void mpu_setup();
 void updateOrientation();
 void getDistance();
 
-void menu0();
-void menu1();
-
 void setup()
 {
   Serial.begin(115200);
+
   lcd.init();
   lcd.backlight();
   lcd.setCursor(2, 0);
   lcd.print("Spirit Level");
   lcd.setCursor(1, 1);
   lcd.print("& Rangefinder");
+
   Serial.println(F("Spirit level with distance meeasurment")); 
   Serial.println(F("Setting up IMU..."));
   mpu_setup();
 
   pinMode(12, OUTPUT); // Status LEDs
   pinMode(13, OUTPUT);
+
   Serial.println(F("Setup complete"));
 
   delay(500);
   lcd.clear();
-  //test change
 }
 
 void loop()
 {
-  updateOrientation();  
+  //updateOrientation(); // Get orientation froM IMU
   if(millis() - lastPingTime >= sonarInterval) // Measure distance every 50ms
   {
     distance = sonar.ping_cm();
     lastPingTime = millis();
   }
-  switch (menuState)
+
+  // put your main code here, to run repeatedly:
+  if (switchA.getButtonState() == LOW && singlePress == true){
+    singlePress = false;
+    switchMenu = true;
+    //menuState++;
+  }else if (switchA.getButtonState() == HIGH){
+    singlePress = true;
+  }
+
+  switch(menuState)
   {
-  case 0:
-    menu0();
+    case 0:
+      menu0();
     break;
-  case 1:
-    menu1();
+
+    case 1:
+      menu1();
     break;
-  
-  default:
+
+    default:
+      rangeError();
     break;
   }
-  delay(1000);
-  menuState++;
   if (menuState > 1)
   {
     menuState = 0;
   }
+
   #ifdef OUTPUT_DEBUG
-    Serial.print(F("Dist: "));
+    /*Serial.print(F("Dist: "));
     Serial.print(distance);
     Serial.print("  ");
     Serial.print(F("Yaw, Pitch, Roll: "));
@@ -114,7 +138,11 @@ void loop()
     Serial.print("  ");
     Serial.print(ypr[1] * 180/M_PI);
     Serial.print("  ");
-    Serial.println(ypr[2] * 180/M_PI);
+    Serial.print(ypr[2] * 180/M_PI);*/
+
+    Serial.print("Menu: ");
+    Serial.println(menuState);
+    //delay(100);
   #endif
 }
 
@@ -197,30 +225,52 @@ void updateOrientation()
   }
 }
 
-void menu0()
+void menu0() // Distance and angle screen
 {
-  static bool Initialization = false; //this is supposed to make the init function only get called once
-
-  if(millis() - lastPingTime >= sonarInterval) // Measure distance every 50ms
-  {
-    distance = sonar.ping_cm();
-    lastPingTime = millis();
-  }
-  updateOrientation();
-  if(!Initialization)
+  if(!init0)
   {
     lcd.clear();
-    lcd.print("Dist:00");
+    lcd.print("Dist:000cm");
     lcd.setCursor(0, 1);
     lcd.print("Anlge:000");
     lcd.print((char)0xDF); // Degree symbol
-    Initialization = true;
+    init0 = true;
   }
+
+  // if button is pressed change init var
+  if(switchMenu == true)
+  {
+    switchMenu = false;
+    init0 = false;
+    menuState++;
+  }
+
+  distanceBuff = distance * 10;
+  lcd.setCursor(5, 0);
+  lcd.print(abs(distanceBuff)/1000); // print first digit
+  lcd.print(abs(distanceBuff)/100 % 10); // print second digit
+  lcd.print(abs(distanceBuff)/10 % 10); // print third digit
+  lcd.print("cm  ");
 }
 
 void menu1()
 {
-  
+  if(!init1)
+  {
+    lcd.clear();
+    lcd.print("Boop:123cm");
+    lcd.setCursor(0, 1);
+    lcd.print("Apple:456");
+    lcd.print((char)0xDF); // Degree symbol
+    init1 = true;
+  }
+
+  if(switchMenu == true)
+  {
+    switchMenu = false;
+    init1 = false;
+    menuState++;
+  }
 }
 
 void rangeError()
